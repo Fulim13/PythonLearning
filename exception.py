ef4b8a8b67f53947a802cd10913648e2a7b3e477
Fulim13 <87457764+Fulim13@users.noreply.github.com>
2023-01-24 17:43:58 +0800

    complete intro to exception

---
+++ exception.py
@@ -0,0 +1,21 @@
+# exception happens, programming mistake. bad data send form user, resource is not avaiable
+# OPEN FILE DOES NOT EXIST , OUR PROGRAM WILL CRASH
+
+
+# numbers = [1, 2]
+# Index Error
+# print(numbers[3])
+# Traceback (most recent call last):
+#   File "c:\Users\ASUS\Desktop\Python\exception.py", line 6, in <module>
+#     print(numbers[3])
+# IndexError: list index out of range
+
+# age = int(input("Age: "))
+# Age: a
+# Traceback (most recent call last):
+#   File "C:\Users\ASUS\Desktop\Python\exception.py", line 13, in <module>
+#     age = int(input("Age: "))
+# ValueError: invalid literal for int() with base 10: 'a'
+
+
+#######################################################################
