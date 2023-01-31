677cf1b2bb00ab67aa047f2940de3d7c3afd110a
Fulim13 <87457764+Fulim13@users.noreply.github.com>
2023-02-01 02:06:18 +0800

    a

--- module/ecommerce/shopping/sales.py
+++ module/ecommerce/shopping/sales.py
@@ -1,13 +1,16 @@
 # intra
+# from ecommerce.customer import contact
 # abs - recommend using
-from ecommerce.customer import contact
 
 # relative
 # from ..customer import contact
 
-contact.contact_customer()
+# contact.contact_customer()
 
-#
+import sys
+print(sys.path)
+# ['c:\\Users\\ASUS\\Desktop\\PythonLearning\\module\\ecommerce\\shopping', 'C:\\Users\\ASUS\\AppData\\Local\\Programs\\Python\\Python310\\python310.zip', 'C:\\Users\\ASUS\\AppData\\Local\\Programs\\Python\\Python310\\DLLs', 'C:\\Users\\ASUS\\AppData\\Local\\Programs\\Python\\Python310\\lib', 'C:\\Users\\ASUS\\AppData\\Local\\Programs\\Python\\Python310', 'C:\\Users\\ASUS\\.virtualenvs\\PythonLearning-ic4l0W3s', 'C:\\Users\\ASUS\\.virtualenvs\\PythonLearning-ic4l0W3s\\lib\\site-packages']
+# it only find in shopping folder, so it can not found the package, so you need to download it
 
 
 def calc_tax():
